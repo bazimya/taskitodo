c537eb07aa388b82f870bda14587d9262c23c520
Placide <wswapped@gmail.com>
2019-02-08 00:43:21 +0200

    Clean up

--- passwordLocker.py
+++ passwordLocker.py
@@ -1,20 +1,8 @@
 import argparse
 import requests
-from firebase import firebase
+from user import user
 
 parser = argparse.ArgumentParser()
 parser.add_argument("username", help="Enter name")
 parser.add_argument("password", help="Enter password")
 args = parser.parse_args()
-
-# Communication to the server
-class passwordLocked:
-	def __init__(self, username, password):
-
-		# ask firebase for this credentials
-		print("Asking firebase, "+username)		
-
-		firebase = firebase.FirebaseApplication('https://learnbase-baa6d.firebaseio.com/', None)
-		result = firebase.get('/forums', None)
-		print(result)
-		# response = requests.post(self.apiAddress, data = {'action':'carExit', 'carPlate':plateNumber, 'parkId':parking, 'cameraId':camera})
---
+++ user.py
@@ -0,0 +1,15 @@
+from firebase import firebase
+class user:
+	def login(self, username, password):
+
+		""""
+			Function to login the user
+		"""
+
+		# ask firebase for this credentials
+		print("Asking firebase, "+username)		
+
+		firebase = firebase.FirebaseApplication('https://learnbase-baa6d.firebaseio.com/', None)
+		result = firebase.get('/forums', None)
+		print(result)
+		# response = requests.post(self.apiAddress, data = {'action':'carExit', 'carPlate':plateNumber, 'parkId':parking, 'cameraId':camera})
